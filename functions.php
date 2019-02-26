26151042438744ef62aa2efb34c4c6d1c81ecabf
Tom Woodward <twwoodward@me.com>
2019-02-26 09:19:20 -0500

    acf stuff added for temporary testing
    remember to turn on acf and ACF to REST API

--- functions.php
+++ functions.php
@@ -167,3 +167,9 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+
+/**
+ * TEMP HOUSING OF ACF FOR TESTING W/O IMPORT HASSLE
+ */
+require get_template_directory() . '/inc/acf-functions.php';
+
