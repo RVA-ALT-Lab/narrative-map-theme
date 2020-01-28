6c70c6246e5de28165073488135e544b82a3d8cb
Jeff Everhart <Jeffeverhart383@gmail.com>
2020-01-28 13:02:39 -0500

    update min and max zoom options

--- js/narrative-map-theme.js
+++ js/narrative-map-theme.js
@@ -11,15 +11,11 @@ var MapUtilityClass = function ($) {
   }
 
   this.initMap = function ( ) {
-      var mymap = L.map('map').setView([37.5215, -78.8537], 7);
-
-
-      // L.tileLayer('https://api.mapbox.com/styles/v1/jeffeverhart383/cj9sxi40c2g3s2skby2y6h8jh/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6IjIwNzVlOTA3ODI2MTY0MjM3OTgxMTJlODgzNjg5MzM4In0.QA1GsfWZccIB8u0FbhJmRg', {
-      //     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      //     maxZoom: 18,
-      //     id: 'mapbox.streets',
-      //     accessToken: window.NARRATIVE_MAP_OPTIONS.mapboxAccessKey
-      // }).addTo(mymap);
+      var mymap = L.map('map', {
+        minZoom: 7,
+        maxZoom: 10
+      })
+      .setView([37.5215, -78.8537])
 
       //TODO: Add check here for localized WP scripts, and setMaxBounds if set
       if (true){
